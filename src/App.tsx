d36773c070b5f75bad337c8b891f2a47d8923fca
daegeun <daegeunkim92@gmail.com>
2021-12-29 12:20:51 +0900

    조건부 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import List, { ItemType } from './components/List';
 const App = () => {
   // useState hook
   const [items, setItems] = useState<ItemType[]>([])
+  const [isShown, setIsShown] = useState<boolean>(false)
 
  //이벤트 처리 함수 생성
   const handleListAddClick = (): void => {
@@ -16,6 +17,10 @@ const App = () => {
     setItems([...items, item])
   }
 
+  const handleIsShownClick = (): void => {
+    setIsShown(!isShown)
+  }
+
   return (
     <div className="app">
       <div>
@@ -23,6 +28,16 @@ const App = () => {
         <List items={items}></List>
         {/* 이벤트 처리 */}
         <button onClick={handleListAddClick}>add</button>
+      </div>
+      <div>
+        {/* 조건부 렌더링 */}
+        {
+          isShown && <p>fixed</p>
+        }
+        {
+          isShown ? <p>true</p> : <p>false</p>
+        }
+        <button onClick={handleIsShownClick}>show</button>
       </div>
     </div>
   );
