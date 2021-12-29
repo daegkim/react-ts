16fcb6d4437caa2a4096b5032b3cd08a2c885e01
daegeun <daegeunkim92@gmail.com>
2021-12-29 12:20:06 +0900

    1. 이벤트 처리 2. setState 처리를 통한 리렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,23 @@ const App = () => {
   // useState hook
   const [items, setItems] = useState<ItemType[]>([])
 
+ //이벤트 처리 함수 생성
+  const handleListAddClick = (): void => {
+    const item = {
+      id: (items.length + 1).toString(),
+      content: (items.length + 1).toString()
+    }
+    //set state를 통해서 리렌더링
+    setItems([...items, item])
+  }
+
   return (
     <div className="app">
       <div>
         {/* props 전달 */}
         <List items={items}></List>
+        {/* 이벤트 처리 */}
+        <button onClick={handleListAddClick}>add</button>
       </div>
     </div>
   );
