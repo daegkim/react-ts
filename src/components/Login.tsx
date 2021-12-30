e52a8d57e0f43d28c097da0f264b85443c956b9c
daegeun <daegeunkim92@gmail.com>
2021-12-30 14:54:45 +0900

    Login후에 input을 초기화시키려함. pwd input은 따로 state를 연결하지 않아서 ref로 직접 DOM 컨트롤함.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,20 @@
-import React, { useContext } from "react"
+import React, { useContext, useRef } from "react"
 import LoginContext, { LoginContextType } from "../contexts/LoginContext";
 
 
 const Login = () => {
   const context = useContext<LoginContextType>(LoginContext)
+  const pwdRef = useRef<HTMLInputElement>(null);
 
   const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     context.setIsLogin(!context.isLogin)
+    // 초기값이 null일수도 있기 때문에 아래와 같은 조건문이 있어야 value를 초기화 시킬 수 있음
+    if(pwdRef.current !== null){
+      //pwdRef가 가리키는 html element의 DOM을 직접 컨트롤
+      pwdRef.current.value = ''
+    }
+    context.setUserId('')
   }
 
   const handleChangeId = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,8 +23,9 @@ const Login = () => {
 
   return (
     <form onSubmit={handleLogin}>
-      <input placeholder="id" type="text" onChange={handleChangeId}></input>
-      <input placeholder="pwd" type="password"></input>
+      <input placeholder="id" type="text" value={context.userId} onChange={handleChangeId}></input>
+      {/* input의 속성에 pwdRef를 연결 */}
+      <input placeholder="pwd" ref={pwdRef}type="password"></input>
       <button type="submit">{ context.isLogin ? 'logout' : 'login' }</button>
     </form>
   )
